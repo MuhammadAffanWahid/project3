5878447e9fcbdb45ad490be717b2c465e587137c
Muhammad Affan <mohammadaffanwahid@gmail.com>
2025-06-04 22:27:29 +0500

    updated routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,19 @@
-from flask import Blueprint, render_template, request, redirect, url_for, current_app
+from flask import Blueprint, request, redirect, url_for, current_app
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     users = current_app.db.users.find()
-    return render_template('index.html', users=users)
+    user_list = ''.join([
+        f"<li>{user['name']} ({user['email']}) - <a href='/delete/{user['_id']}'>Delete</a></li>"
+        for user in users
+    ])
+    return f'''
+        <h1>Users</h1>
+        <a href="/add">Add User</a>
+        <ul>{user_list}</ul>
+    '''
 
 @main.route('/add', methods=['GET', 'POST'])
 def add_user():
@@ -14,7 +22,16 @@ def add_user():
         email = request.form['email']
         current_app.db.users.insert_one({'name': name, 'email': email})
         return redirect(url_for('main.index'))
-    return render_template('add_user.html')
+
+    return '''
+        <h1>Add User</h1>
+        <form method="POST">
+            Name: <input type="text" name="name" required><br>
+            Email: <input type="email" name="email" required><br>
+            <input type="submit" value="Add">
+        </form>
+        <a href="/">Back</a>
+    '''
 
 @main.route('/delete/<user_id>')
 def delete_user(user_id):
